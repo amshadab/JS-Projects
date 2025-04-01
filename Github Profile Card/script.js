c863f383741c6aa163c770fc6869dc75b266cefa
Shadab Ansari <shadabansari8319@gmail.com>
2025-04-01 12:15:56 +0530

    project github

---
+++ Github Profile Card/script.js
@@ -0,0 +1,33 @@
+const send = document.getElementById('send')
+const username=document.getElementById('username')
+let url
+send.addEventListener('click',function(e){
+    url = `https://api.github.com/users/${username.value}`
+   
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET',url)
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState===4){
+            const data = JSON.parse(this.responseText)
+            const followers = data.followers
+            const following = data.following
+            const profile= data.avatar_url
+            const name = data.name
+            const bio = data.bio
+
+            document.querySelector('.avatar').src = profile;
+            document.querySelector('.username').innerHTML=name;
+            document.querySelector('#bio').innerHTML=bio;
+            document.querySelector('#followers').innerHTML=followers;
+            document.querySelector('#following').innerHTML=following
+
+            document.querySelector('.card').style.display="block";
+
+
+            }
+    }
+    xhr.send()
+})
+
+// console.log("Hello")
+   
